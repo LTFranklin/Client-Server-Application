3e03f6f670dd61e31b400b35719cd0e1e7ef52eb
LTFranklin <louis.t.franklin@gmail.com>
2017-02-17 12:31:50 +0000

    Everything so far

---
+++ Networking/location/location/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.IO;
+using System.Net;
+
+namespace location
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpClient client = new TcpClient();
+            client.Connect("localhost", 43);
+            //client.Connect("whois.networksolutions.com", 43);
+            //client.Connect("whois.net.dcs.hull.ac.uk", 43);
+            StreamWriter streamWrite = new StreamWriter(client.GetStream());
+            StreamReader streamRead = new StreamReader(client.GetStream());
+            NetworkStream netStream = client.GetStream();
+
+            try
+            {
+                if (args[args.Length - 1] == "/h9")
+                {
+                    h09Pro(args, streamWrite);
+                }
+                else if (args[args.Length - 1] == "/h0")
+                {
+                    h10Pro(args, streamWrite);
+                }
+                else if (args[args.Length - 1] == "/h1")
+                {
+                    h11Pro(args, streamWrite);
+                }
+                else
+                {
+                    whoisPro(args, streamWrite);
+                }
+
+                streamWrite.Flush();
+                Console.WriteLine(streamRead.ReadToEnd());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        static public void whoisPro(string[] args, StreamWriter streamWrite)
+        {
+            if (args.Length == 2)
+            {
+                streamWrite.Write(args[0] + " " + args[1] + "\n");
+            }
+            else
+            {
+                streamWrite.Write(args[0] + "\n");
+            }
+        }
+
+        static public void h09Pro(string[] args, StreamWriter streamWrite)
+        {
+            if (args.Length == 3)
+            {
+                streamWrite.Write("PUT /" + args[0] + "\n\n" + args[1] +"\n");
+            }
+            else
+            {
+                streamWrite.Write("GET /" + args[0] + "\n");
+            }
+        }
+
+        static public void h10Pro(string[] args, StreamWriter streamWrite)
+        {
+            if (args.Length == 3)
+            {
+                streamWrite.Write("POST /" + args[0] + " HTTP/1.0\nContent-Length: " + args[1].Length + "\n" + args[1]);
+            }
+            else
+            {
+                streamWrite.Write("GET /?" + args[0] + " HTTP/1.0\n");
+            }
+        }
+
+        static public void h11Pro(string[] args, StreamWriter streamWrite)
+        {
+            if (args.Length == 3)
+            {
+                streamWrite.Write("POST / HTTP/1.1\nHost: " + Dns.GetHostName() + "\nContent-Length: " + (args[0].Length+args[1].Length) + "\nname=" + args[0] + "&location=" + args[1]);
+            }
+            else
+            {
+                streamWrite.Write("GET /?name=" + args[0] + " HTTP/1.1\nHost: " + Dns.GetHostName() + "\n");
+            }
+        }
+    }
+}
---
+++ Networking/locationServer/locationServer/Program.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace locationServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> dataStore = new Dictionary<string, string>();
+            dataStore.Add("tim", "here");
+            runServer(dataStore);
+        }
+
+        static public void runServer(Dictionary<string, string> dataStore)
+        {
+            TcpListener listener;
+            Socket connection;
+            NetworkStream socketStream;
+            TcpClient client;
+            IPAddress localIP = Dns.Resolve("localhost").AddressList[0];
+
+            try
+            {
+                listener = new TcpListener(localIP, 43);
+                listener.Start();
+                while (true)
+                {
+                    Console.WriteLine("Listening");
+                    connection = listener.AcceptSocket();
+                    client = listener.AcceptTcpClient();
+                    socketStream = client.GetStream();
+                    doRequest(client, dataStore);
+                    socketStream.Close();
+                    connection.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        static public Dictionary<string, string> doRequest(TcpClient client, Dictionary<string, string> dataStore)
+        {
+            StreamReader streamRead = new StreamReader(client.GetStream());
+            StreamWriter streamWrite = new StreamWriter(client.GetStream());
+            string recieved = "";
+            char[] rawIn = new char[250]; ;
+            streamRead.Read(rawIn, 0, 250);
+            for(int i = 0; i < 250; i++)
+            {
+                recieved = recieved + rawIn[i];
+            }
+            Console.WriteLine(recieved);
+            match(recieved, dataStore, streamWrite);
+            streamWrite.Flush();
+            return dataStore;
+        }
+
+
+        static public void match(string rawInput, Dictionary<string, string> dataStore, StreamWriter streamWrite)
+        {
+            string whoisFind = @"^\b([A-z0-9]+)\n";
+            string whoisEdit = @"^\b([A-z0-9]+) ([A-z0-9 ]+)\n";
+            string http09Find = @"^\bGET /([A-z0-9]+)\n";
+            string http09Edit = @"^\bPUT /([A-z0-9]+)\n\n([A-z0-9 ]+)\n";
+            string http10Find = @"^GET /\?([A-z0-9]+) HTTP/1.0[\nA-z0-9]*\n";
+            string http10Edit = @"^POST /([A-z0-9]+) HTTP/1.0\nContent-Length: [0-9]+[\nA-z0-9]*\n([A-z0-9]+)";
+            string http11Find = @"GET /\?name=([A-z0-9]+) HTTP/1.1\nHost: [A-z0-9\-]+\n";
+            string http11Edit = @"^POST / HTTP/1.1\nHost: [A-z0-9\-]+\nContent-Length: [0-9]+[\nA-z0-9]*\nname=([A-z0-9]+)&location=([A-z0-9]+)";
+
+
+            if (Regex.IsMatch(rawInput, whoisFind))
+            {
+                foreach(Match info in Regex.Matches(rawInput, whoisFind))
+                {
+                    string record = (findRecord(dataStore, info.Groups[1].ToString()));
+                    if (record == "fail")
+                    {
+                        streamWrite.Write("ERROR: no entries found\n");
+                    }
+                    else
+                    {
+                        streamWrite.Write(record + "\n");
+                    }
+                }
+            }
+            if (Regex.IsMatch(rawInput, whoisEdit))
+            {
+                foreach (Match info in Regex.Matches(rawInput, whoisEdit))
+                {
+                    editRecord(dataStore, info.Groups[1].ToString(), info.Groups[2].ToString());
+                    streamWrite.Write("OK\n");
+                }
+            }
+            if (Regex.IsMatch(rawInput, http09Find))
+            {
+                foreach (Match info in Regex.Matches(rawInput, http09Find))
+                {
+                    string record = (findRecord(dataStore, info.Groups[1].ToString()));
+                    if (record == "fail")
+                    {
+                        streamWrite.Write("HTTP/0.9 404 Not Found\nContent-Type: text/plain\n\n");
+                    }
+                    else
+                    {
+                        streamWrite.Write("HTTTP/0.9 200 OK\nContent-Type: text/plain\n\n" + record + "\n");
+                    }
+                }
+            }
+            if (Regex.IsMatch(rawInput, http09Edit))
+            {
+                foreach (Match info in Regex.Matches(rawInput, http09Edit ))
+                {
+                    editRecord(dataStore, info.Groups[1].ToString(), info.Groups[2].ToString());
+                    streamWrite.Write("HTTP/0.9 200 OK\nContent-Type: text/plain\n\n");
+                }
+            }
+            if (Regex.IsMatch(rawInput, http10Find))
+            {
+                foreach(Match info in Regex.Matches(rawInput, http10Find))
+                {
+                    string record = (findRecord(dataStore, info.Groups[1].ToString()));
+                    if (record == "fail")
+                    {
+                        streamWrite.Write("HTTP/1.0 404 Not Found\nContent-Type: text/plain\n\n");
+                    }
+                    else
+                    {
+                        streamWrite.Write("HTTTP/1.0 200 OK\nContent-Type: text/plain\n\n" + record + "\n");
+                    }
+                }
+            }
+            if (Regex.IsMatch(rawInput, http10Edit))
+            {
+                foreach (Match info in Regex.Matches(rawInput, http10Edit))
+                {
+                    editRecord(dataStore, info.Groups[1].ToString(), info.Groups[2].ToString());
+                    streamWrite.Write("HTTP/1.0 200 OK\nContent-Type: text/plain\n\n");
+                }
+            }
+            if (Regex.IsMatch(rawInput, http11Find))
+            {
+                foreach (Match info in Regex.Matches(rawInput, http11Find))
+                {
+                    string record = (findRecord(dataStore, info.Groups[1].ToString()));
+                    if (record == "fail")
+                    {
+                        streamWrite.Write("HTTP/1.1 404 Not Found\nContent-Type: text/plain\n\n");
+                    }
+                    else
+                    {
+                        streamWrite.Write("HTTTP/1.1 200 OK\nContent-Type: text/plain\n\n" + record + "\n");
+                    }
+                }
+            }
+            if (Regex.IsMatch(rawInput, http11Edit))
+            {
+                foreach (Match info in Regex.Matches(rawInput, http11Edit))
+                {
+                    editRecord(dataStore, info.Groups[1].ToString(), info.Groups[2].ToString());
+                    streamWrite.Write("HTTP/1.1 200 OK\nContent-Type: text/plain\n\n");
+                }
+            }
+        }
+
+        static public Dictionary<string,string> editRecord(Dictionary<string,string> dataStore, string name, string location)
+        {
+            if (dataStore.ContainsKey(name))
+            {
+                dataStore[name] = location;
+            }
+            else
+            {
+                dataStore.Add(name, location);
+            }
+            return dataStore;
+        }
+
+        static public string findRecord(Dictionary<string, string> dataStore, string name)
+        {
+            if (dataStore.ContainsKey(name))
+            {
+                return dataStore[name];
+            }
+            else
+            {
+                return "fail";
+            }
+        }
+    }
+}
