bdef742347545009b5300fa4ce851e0cac45f34e
LTFranklin <louis.t.franklin@gmail.com>
2017-02-20 00:52:45 +0000

    added client side parsing and more commands
    /h and /p not yet tested

--- Networking/locationServer/locationServer/Program.cs
+++ Networking/locationServer/locationServer/Program.cs
@@ -22,7 +22,7 @@ namespace locationServer
         static public void runServer(Dictionary<string, string> dataStore)
         {
             TcpListener listener;
-            Socket connection;
+            //Socket connection;
             NetworkStream socketStream;
             TcpClient client;
             IPAddress localIP = Dns.Resolve("localhost").AddressList[0];
@@ -34,12 +34,14 @@ namespace locationServer
                 while (true)
                 {
                     Console.WriteLine("Listening");
-                    connection = listener.AcceptSocket();
+                    //connection = listener.AcceptSocket();
+                    //Console.WriteLine(connection.Connected.ToString());
                     client = listener.AcceptTcpClient();
                     socketStream = client.GetStream();
                     doRequest(client, dataStore);
                     socketStream.Close();
-                    connection.Close();
+                    //connection.Close();
+                    //Console.WriteLine(connection.Connected.ToString());
                 }
             }
             catch (Exception e)
@@ -62,20 +64,21 @@ namespace locationServer
             Console.WriteLine(recieved);
             match(recieved, dataStore, streamWrite);
             streamWrite.Flush();
+            streamWrite.Close();
             return dataStore;
         }
 
 
         static public void match(string rawInput, Dictionary<string, string> dataStore, StreamWriter streamWrite)
         {
-            string whoisFind = @"^\b([A-z0-9]+)\n";
-            string whoisEdit = @"^\b([A-z0-9]+) ([A-z0-9 ]+)\n";
-            string http09Find = @"^\bGET /([A-z0-9]+)\n";
-            string http09Edit = @"^\bPUT /([A-z0-9]+)\n\n([A-z0-9 ]+)\n";
-            string http10Find = @"^GET /\?([A-z0-9]+) HTTP/1.0[\nA-z0-9]*\n";
-            string http10Edit = @"^POST /([A-z0-9]+) HTTP/1.0\nContent-Length: [0-9]+[\nA-z0-9]*\n([A-z0-9]+)";
-            string http11Find = @"GET /\?name=([A-z0-9]+) HTTP/1.1\nHost: [A-z0-9\-]+\n";
-            string http11Edit = @"^POST / HTTP/1.1\nHost: [A-z0-9\-]+\nContent-Length: [0-9]+[\nA-z0-9]*\nname=([A-z0-9]+)&location=([A-z0-9]+)";
+            string whoisFind = @"^\b([A-z0-9 ]+)\n";
+            string whoisEdit = @"^\b([A-z0-9 ]+) ([A-z0-9 ]+)\n";
+            string http09Find = @"^\bGET /([A-z0-9 ]+)\r\n";
+            string http09Edit = @"^\bPUT /([A-z0-9 ]+)\r\n\r\n([A-z0-9 ]+)\r\n";
+            string http10Find = @"^GET /\?([A-z0-9 ]+) HTTP/1.0[\nA-z0-9 ]*\r\n";
+            string http10Edit = @"^POST /([A-z0-9 ]+) HTTP/1.0\r\nContent-Length: [0-9]+[\r\nA-z0-9 ]*\r\n([A-z0-9 ]+)";
+            string http11Find = @"GET /\?name=([A-z0-9 ]+) HTTP/1.1\nHost: [A-z0-9\- ]+\n";
+            string http11Edit = @"^POST / HTTP/1.1\nHost: [A-z0-9\- ]+\nContent-Length: [0-9]+[\nA-z0-9 ]*\nname=([A-z0-9 ]+)&location=([A-z0-9]+)";
 
 
             if (Regex.IsMatch(rawInput, whoisFind))
@@ -85,89 +88,89 @@ namespace locationServer
                     string record = (findRecord(dataStore, info.Groups[1].ToString()));
                     if (record == "fail")
                     {
-                        streamWrite.Write("ERROR: no entries found\n");
+                        streamWrite.Write("ERROR: no entries found\r\n");
                     }
                     else
                     {
-                        streamWrite.Write(record + "\n");
+                        streamWrite.Write(record + "\r\n");
                     }
                 }
             }
-            if (Regex.IsMatch(rawInput, whoisEdit))
+            else if (Regex.IsMatch(rawInput, whoisEdit))
             {
                 foreach (Match info in Regex.Matches(rawInput, whoisEdit))
                 {
                     editRecord(dataStore, info.Groups[1].ToString(), info.Groups[2].ToString());
-                    streamWrite.Write("OK\n");
+                    streamWrite.Write("OK\r\n");
                 }
             }
-            if (Regex.IsMatch(rawInput, http09Find))
+            else if (Regex.IsMatch(rawInput, http09Find))
             {
                 foreach (Match info in Regex.Matches(rawInput, http09Find))
                 {
                     string record = (findRecord(dataStore, info.Groups[1].ToString()));
                     if (record == "fail")
                     {
-                        streamWrite.Write("HTTP/0.9 404 Not Found\nContent-Type: text/plain\n\n");
+                        streamWrite.Write("HTTP/0.9 404 Not Found\r\nContent-Type: text/plain\r\n\r\n");
                     }
                     else
                     {
-                        streamWrite.Write("HTTTP/0.9 200 OK\nContent-Type: text/plain\n\n" + record + "\n");
+                        streamWrite.Write("HTTP/0.9 200 OK\r\nContent-Type: text/plain\r\n\r\n" + record + "\r\n");
                     }
                 }
             }
-            if (Regex.IsMatch(rawInput, http09Edit))
+            else if (Regex.IsMatch(rawInput, http09Edit))
             {
                 foreach (Match info in Regex.Matches(rawInput, http09Edit ))
                 {
                     editRecord(dataStore, info.Groups[1].ToString(), info.Groups[2].ToString());
-                    streamWrite.Write("HTTP/0.9 200 OK\nContent-Type: text/plain\n\n");
+                    streamWrite.Write("HTTP/0.9 200 OK\r\nContent-Type: text/plain\r\n\r\n");
                 }
             }
-            if (Regex.IsMatch(rawInput, http10Find))
+            else if (Regex.IsMatch(rawInput, http10Find))
             {
                 foreach(Match info in Regex.Matches(rawInput, http10Find))
                 {
                     string record = (findRecord(dataStore, info.Groups[1].ToString()));
                     if (record == "fail")
                     {
-                        streamWrite.Write("HTTP/1.0 404 Not Found\nContent-Type: text/plain\n\n");
+                        streamWrite.Write("HTTP/1.0 404 Not Found\r\nContent-Type: text/plain\r\n\r\n");
                     }
                     else
                     {
-                        streamWrite.Write("HTTTP/1.0 200 OK\nContent-Type: text/plain\n\n" + record + "\n");
+                        streamWrite.Write("HTTP/1.0 200 OK\r\nContent-Type: text/plain\r\n\r\n" + record + "\r\n");
                     }
                 }
             }
-            if (Regex.IsMatch(rawInput, http10Edit))
+            else if (Regex.IsMatch(rawInput, http10Edit))
             {
                 foreach (Match info in Regex.Matches(rawInput, http10Edit))
                 {
                     editRecord(dataStore, info.Groups[1].ToString(), info.Groups[2].ToString());
-                    streamWrite.Write("HTTP/1.0 200 OK\nContent-Type: text/plain\n\n");
+                    streamWrite.Write("HTTP/1.0 200 OK\r\nContent-Type: text/plain\r\n\r\n");
                 }
             }
-            if (Regex.IsMatch(rawInput, http11Find))
+            else if (Regex.IsMatch(rawInput, http11Find))
             {
                 foreach (Match info in Regex.Matches(rawInput, http11Find))
                 {
                     string record = (findRecord(dataStore, info.Groups[1].ToString()));
                     if (record == "fail")
                     {
-                        streamWrite.Write("HTTP/1.1 404 Not Found\nContent-Type: text/plain\n\n");
+                        streamWrite.Write("HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\n\r\n");
                     }
                     else
                     {
-                        streamWrite.Write("HTTTP/1.1 200 OK\nContent-Type: text/plain\n\n" + record + "\n");
+                        streamWrite.Write("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\n" + record + "\r\n");
                     }
                 }
             }
-            if (Regex.IsMatch(rawInput, http11Edit))
+            else if (Regex.IsMatch(rawInput, http11Edit))
             {
                 foreach (Match info in Regex.Matches(rawInput, http11Edit))
                 {
                     editRecord(dataStore, info.Groups[1].ToString(), info.Groups[2].ToString());
-                    streamWrite.Write("HTTP/1.1 200 OK\nContent-Type: text/plain\n\n");
+                    streamWrite.Write("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\n");
                 }
             }
         }
